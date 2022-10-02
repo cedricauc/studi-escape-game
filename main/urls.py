a93191b75ab85bef3d4dbf34002d65b06109dbd8
Cedric Auclair <39864243+cedricauc@users.noreply.github.com>
2022-10-02 18:24:22 +0200

    T-18: dev-front page details

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("scenario/<str:id>", views.details, name="details"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "main/index.html")
+
+
+def details(request, id):
+    return render(request, "main/details.html")
